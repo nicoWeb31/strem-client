76c391ffbb92dc0602aed0c3adb3fca3ad740774
nicoWeb31 <nico.riot@free.fr>
2020-09-26 19:03:36 +0200

    header

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './header/Header';
+import StreamCreate from './streams/StreamCreate/StreamCreate';
+import StreamDelete from './streams/StreamDelete/StreamDelete';
+import StreamEdit from './streams/StreamEdit/StreamEdit';
+import StreamList from './streams/StreamList/StreamList';
+import StreamShow from './streams/StreamShow/StreamShow';
 
 const App = () => {
     return (
         <BrowserRouter>
+            <div className='ui container'>
 
-        <div>
+                <Header />
 
-        </div>
+                <Route path='/' exact component={StreamList} />
+                <Route path='/stream/new' exact component={StreamCreate} />
+                <Route path='/stream/edit' exact component={StreamEdit} />
+                <Route path='/stream/delete' exact component={StreamDelete} />
+                <Route path='/stream/show' exact component={StreamShow} />
+
+
+            </div>
 
 
         </BrowserRouter>
