c0426139ce775d713840a674c988dbfb97086a5c
nicoWeb31 <nico.riot@free.fr>
2020-09-28 20:20:00 +0200

    implementation redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import reducers from './reducers'
 
 
 import App from './component/App';
 
 
-
-const store = createStore(reducers)
+const composeEnHancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnHancers(applyMiddleware())
+    )
 
 ReactDom.render(
     <Provider store={store}>
