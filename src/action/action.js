9d1af67b6304b52e16d92b31f4f09c7ecaacd56a
nicoWeb31 <nico.riot@free.fr>
2020-09-28 18:12:18 +0200

    implementation redux

---
+++ src/action/action.js
@@ -0,0 +1,13 @@
+
+export const signIn = () =>{
+    return {
+        type: "SIGN_IN"
+    }
+}
+
+
+export const singnOut = () =>{
+    return {
+        type: 'SIGN_OUT'
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers'
 
-import App from './component/App'
+
+import App from './component/App';
+
+
+
+const store = createStore(reducers)
 
 ReactDom.render(
-    <App/>,
+    <Provider store={store}>
+    <App/>
+    </Provider>
+        
+    ,
     document.querySelector('#root')
 )
