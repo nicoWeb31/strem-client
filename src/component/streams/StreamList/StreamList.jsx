6b26a1fde112fc46b2d7699f5e590f255662b949
nicoWeb31 <nico.riot@free.fr>
2020-09-30 17:03:38 +0200

    render stream list

--- src/component/streams/StreamList/StreamList.jsx
+++ src/component/streams/StreamList/StreamList.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from 'react'
 
-import { connect  } from 'react-redux';
-import {getAllStream} from '../../../action/action'
+import { connect } from 'react-redux';
+import { getAllStream } from '../../../action/action'
 
 class StreamList extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {};
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.getAllStream()
+    }
+
+
+    renderList = () => {
+
+        return this.props.streams.map(stream => {
+            return (
+                <div className='item' key={stream.id}>
+                    <i className='large middle aligned icon camera' />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            )
+        })
     }
 
     render() {
@@ -19,7 +37,10 @@ class StreamList extends Component {
 
         return (
             <div>
-            <h1>list stream</h1>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderList()}
+                </div>
             </div>
 
         );
@@ -28,9 +49,10 @@ class StreamList extends Component {
 
 
 
-const mapStateToProps = state =>{
-    return {streams : state.streams}
+const mapStateToProps = state => {
+    //object.values renvoie un tableau d'un objet 
+    return { streams: Object.values(state.streams) }
 }
-export default connect(mapStateToProps,{getAllStream})(StreamList);
+export default connect(mapStateToProps, { getAllStream })(StreamList);
 
 
