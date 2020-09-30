a1810b10e04b3650568b0d8d19054eb3ec0e91fa
nicoWeb31 <nico.riot@free.fr>
2020-09-30 12:02:10 +0200

    render streams

--- src/component/streams/StreamList/StreamList.jsx
+++ src/component/streams/StreamList/StreamList.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+import { connect  } from 'react-redux';
+import {getAllStream} from '../../../action/action'
 
-const StreamList = () => {
-    return (
-        <div>list</div>
-    );
+class StreamList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+
+    componentDidMount(){
+        this.props.getAllStream()
+    }
+
+    render() {
+
+        console.log(this.props.streams)
+
+        return (
+            <div>
+            <h1>list stream</h1>
+            </div>
+
+        );
+    }
 }
 
-export default StreamList;
+
+
+const mapStateToProps = state =>{
+    return {streams : state.streams}
+}
+export default connect(mapStateToProps,{getAllStream})(StreamList);
+
+
