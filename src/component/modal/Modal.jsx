d916f18770d229d7474897268a7c226c52dfe033
nicoWeb31 <nico.riot@free.fr>
2020-10-02 12:31:23 +0200

    reutilisable modal

--- src/component/modal/Modal.jsx
+++ src/component/modal/Modal.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import ReactDom from 'react-dom';
 
 
-//custum history for reditect
-import history from '../../history'
 
 
 
-const Modal = ({title,content,actionBtn}) => {
+
+const Modal = ({title,content,actionBtn,pathSkip,actions,onDismiss}) => {
     return ReactDom.createPortal(
         <div className='ui dimmer modals visible active'
-        onClick={()=>history.push('/')}
+        onClick={onDismiss}
         >
             <div className="ui standard modal visible active"
             onClick={(e)=>e.stopPropagation()}
@@ -21,11 +20,16 @@ const Modal = ({title,content,actionBtn}) => {
                 <div className="content">
                     {content}
                 </div>
-                <div className="actions">
-                    <button className ='ui primary button' > {action}</button>
-                    <button className ='ui  button' onClick={()=>history.push('/')}>Cancel</button>
+                {/* <div className="actions">
+                    <button className ='ui primary button' > {actionBtn}</button>
+                    <button className ='ui  button' onClick={()=>history.push(pathSkip)}>Cancel</button>
+
+                </div> */}
+                <div className='actions'>
+                {actions}
 
                 </div>
+
             </div>
         </div>,
         document.querySelector('#modalPortal')
--- src/component/streams/StreamDelete/StreamDelete.jsx
+++ src/component/streams/StreamDelete/StreamDelete.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import Modal from '../../modal/Modal'
 
+//custum history for reditect
+import history from '../../../history'
+
 
 const StreamDelete = () => {
+
+
+    const actions = (
+
+        <React.Fragment>
+            <button className='ui button negative'>Delete</button>
+            <button className='ui button' onClick={()=>history.push('/')}>Cancel</button>
+        </React.Fragment>
+
+    )
+
+    const  onDismiss = () =>{
+        history.push('/')
+    }
+
+
     return (
         <div>
         delete
@@ -11,6 +30,8 @@ const StreamDelete = () => {
             title="Delete stream ?"
             content="Are you sur to delete this stream ?"
             actionBtn="Delete"
+            onDismiss={onDismiss}
+            actions={actions}
         />
         </div>
     );
